7ec950b9383aec856d94f6bb5e12bf3ff515ad05
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2018-01-03 18:28:31 +0100

    something more

--- orangecontrib/prototypes/widgets/owscatterplot.py
+++ orangecontrib/prototypes/widgets/owscatterplot.py
@@ -5,16 +5,14 @@ from typing import List, Sequence, Optional
 
 import numpy as np
 
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QSize, QAbstractListModel
 from PyQt5.QtGui import QStandardItem, QStandardItemModel, QVector3D
 from PyQt5.QtWidgets import (
     QWidget, QFormLayout, QComboBox, QApplication, QGroupBox
 )
 
-
-
 from PyQt5.QtDataVisualization import (
-    QScatterDataItem, QScatter3DSeries, QScatterDataProxy, Q3DScatter
+    QScatterDataItem, QScatter3DSeries, QScatterDataProxy, Q3DScatter, Q3DTheme
 )
 
 import Orange.data
@@ -24,7 +22,16 @@ from Orange.widgets.utils import itemmodels
 
 class Data(SimpleNamespace):
     xyz = ...  # type: np.ndarray
-    mask = ...  # type: nd.ndarray
+    mask = ...  # type: np.ndarray
+
+
+class Models(SimpleNamespace):
+    x = ...  # type: QAbstractListModel
+    y = ...  # type: QAbstractListModel
+    z = ...  # type: QAbstractListModel
+    color = ...  # type: QAbstractListModel
+    shape = ...  # type: QAbstractListModel
+    size = ...   # type: QAbstractListModel
 
 
 class OWScatterPlot(widget.OWWidget):
@@ -51,10 +58,12 @@ class OWScatterPlot(widget.OWWidget):
         self._inputs.data = None  # type: Optional[Orange.data.Table]
         self._inputs.subset = None  # type: Optional[Orange.data.Table]
 
+        self._plotdata = SimpleNamespace()
+
         self.models = SimpleNamespace()
-        self.models.varx = itemmodels.VariableListModel(parent=self)
-        self.models.vary = itemmodels.VariableListModel(parent=self)
-        self.models.varz = itemmodels.VariableListModel(parent=self)
+        self.models.x = itemmodels.VariableListModel(parent=self)
+        self.models.y = itemmodels.VariableListModel(parent=self)
+        self.models.z = itemmodels.VariableListModel(parent=self)
         self.models.color = itemmodels.VariableListModel(parent=self)
         self.models.shape = itemmodels.VariableListModel(parent=self)
         self.models.size = itemmodels.VariableListModel(parent=self)
@@ -66,11 +75,11 @@ class OWScatterPlot(widget.OWWidget):
         )
 
         self.axesgui = SimpleNamespace()
-        self.axesgui.cbx = QComboBox(
+        self.axesgui.x = QComboBox(
             self, objectName="cb-x", activated=self._updateplot)
-        self.axesgui.cby = QComboBox(
-            self, objectName="cb-y", )
-        self.axesgui.cbz = QComboBox(
+        self.axesgui.y = QComboBox(
+            self, objectName="cb-y", activated=self._updateplot)
+        self.axesgui.z = QComboBox(
             self, objectName="cb-z", activated=self._updateplot)
         self.axesgui.color = QComboBox(
             self, objectName="cb-color", activated=self._updateplot)
@@ -79,13 +88,15 @@ class OWScatterPlot(widget.OWWidget):
         self.axesgui.size = QComboBox(
             self, objectName="cb-size", activated=self._updateplot)
 
-        self.axesgui.cbx.setModel(self.models.varx)
-        self.axesgui.cby.setModel(self.models.vary)
-        self.axesgui.cbz.setModel(self.models.varz)
+        self.axesgui.x.setModel(self.models.x)
+        self.axesgui.y.setModel(self.models.y)
+        self.axesgui.z.setModel(self.models.z)
+        self.axesgui.color.setModel(self.models.color)
+        # self.axesgui.shape.setModel(self.models.shape)
 
-        form.addRow("X", self.axesgui.cbx)
-        form.addRow("Y", self.axesgui.cby)
-        form.addRow("Z", self.axesgui.cbz)
+        form.addRow("X", self.axesgui.x)
+        form.addRow("Y", self.axesgui.y)
+        form.addRow("Z", self.axesgui.z)
 
         form.addRow("Color", self.axesgui.color)
         form.addRow("Shape", self.axesgui.shape)
@@ -96,13 +107,20 @@ class OWScatterPlot(widget.OWWidget):
         self.controlArea.layout().addStretch()
 
         self.__scatter = Q3DScatter()
+        self.__scatter.setShadowQuality(Q3DScatter.ShadowQualityNone)
+        self.__scatter.setOptimizationHints(Q3DScatter.OptimizationStatic)
+        # self.__scatter.setActiveTheme(Q3DTheme(Q3DTheme.ThemeIsabelle))
         self.mainArea.layout().addWidget(
             QWidget.createWindowContainer(self.__scatter)
         )
 
+    def sizeHint(self):
+        return super().sizeHint().expandedTo(QSize(1027, 900))
+
     def clear(self):
-        # self.plot.clear()
-        self._plotdata = None
+        for series in self.__scatter.seriesList():
+            self.__scatter.removeSeries(series)
+        self._plotdata = SimpleNamespace()
 
     def set_data(self, data):
         # type: (Optional[Orange.data.Table]) -> None
@@ -118,23 +136,29 @@ class OWScatterPlot(widget.OWWidget):
             varsdisc = [var for var in primary if var.is_discrete]
             self.variables = primary
             self.auxvariables = aux
-            self.models.varx[:] = primary
-            self.models.vary[:] = primary
-            self.models.varz[:] = primary
+            self.models.x[:] = primary
+            self.models.y[:] = primary
+            self.models.z[:] = primary
 
             self.models.color[:] = primary
             self.models.shape[:] = varsdisc
             self.models.size[:] = primary
 
-            self.axesgui.cbx.setCurrentIndex(0)
-            self.axesgui.cby.setCurrentIndex(1)
-            self.axesgui.cbz.setCurrentIndex(2)
+            self.axesgui.x.setCurrentIndex(0)
+            self.axesgui.y.setCurrentIndex(1)
+            self.axesgui.z.setCurrentIndex(2)
+            self._plotdata.data = None
 
+            def data_(var):
+                values, mask = get_column_data(data, vars)
+                return values, mask
+
+            self._plotdata.data_source = data_
         self._update()
 
     def set_subset_data(self, subset):
-        self.__plotdata.subsetmask = None
-        self.__plotdata.subsetids = None
+        self._plotdata.subsetmask = None
+        self._plotdata.subsetids = None
         if subset is not None:
             self.__plotdata.subsetids = subset.ids
         self._update()
@@ -147,22 +171,118 @@ class OWScatterPlot(widget.OWWidget):
         self._updateplot()
 
     def _updateplot(self):
-        varx = self.axesgui.cbx.currentIndex()
-        vary = self.axesgui.cby.currentIndex()
-        varz = self.axesgui.cbz.currentIndex()
+        xi = self.axesgui.x.currentIndex()
+        yi = self.axesgui.y.currentIndex()
+        zi = self.axesgui.z.currentIndex()
+
+        varx = self.models.x[xi]
+        vary = self.models.y[yi]
+        varz = self.models.z[zi]
 
         x, _ = self.data.get_column_view(varx)
         y, _ = self.data.get_column_view(vary)
         z, _ = self.data.get_column_view(varz)
 
-        array = [QScatterDataItem(QVector3D(*t)) for t in zip(x, y, z)]
-        ser = QScatter3DSeries()
-        ser.dataProxy().addItems(array)
+        ci = self.axesgui.color.currentIndex()
+        color_dims = shape_dims = 1
+        if ci != -1:
+            varcolor = self.models.color[ci]
+            color, _ = self.data.get_column_view(varcolor)
+            if varcolor.is_continuous:
+                mask = np.isfinite(color)
+                color_non_na = color[mask]
+                if color_non_na.size > 0:
+                    cmin, cmax = np.min(color_non_na), np.max(color_non_na)
+                else:
+                    cmin, cmax = 0., 1.0
+                color_dims = 256
+                edges = np.linspace(cmin, cmax, color_dims, endpoint=True)
+                color = np.digitize(color_non_na, bins=edges) - 1
+                color_map = "jet"
+            else:
+                mask = np.isfinite(color)
+                color_non_na = color[mask]
+                color = color.astype(np.intp)
+                color_map = ...
+                color_dims = len(varcolor.values) + 1
+        else:
+            color = np.zeros(x.shape, int)
+
+        varshape = self.axesgui.shape.currentIndex()
+        if varshape != -1:
+            shape, _ = self.data.get_column_view(varshape)
+        else:
+            shape = np.zeros(x.shape, int)
+
+        series = []
+
+        group = np.ravel_multi_index((color, shape), dims=(color_dims, shape_dims))
+        keys, indices = group_by_indices(group)
+        theme = self.__scatter.activeTheme()
+        for group, gindices in zip(keys, indices):
+            color_i = group // color_dims
+            shape_i = group % color_dims
+            array = [QScatterDataItem(QVector3D(*t))
+                     for t in zip(x[gindices], y[gindices], z[gindices])]
+            ser = QScatter3DSeries()
+            ser.setItemLabelFormat(
+                "@xTitle: @xLabel @yTitle: @yLabel @zTitle: @zLabel"
+            )
+            ser.setMeshSmooth(True)
+            ser.dataProxy().addItems(array)
+            series.append(ser)
+
+        self.__scatter.axisX().setTitle(self.axesgui.x.currentText())
+        self.__scatter.axisY().setTitle(self.axesgui.y.currentText())
+        self.__scatter.axisZ().setTitle(self.axesgui.z.currentText())
+
+        # array = [QScatterDataItem(QVector3D(*t)) for t in zip(x, y, z)]
+        # ser = QScatter3DSeries()
+        # ser.setItemLabelFormat(
+        #     "@xTitle: @xLabel @yTitle: @yLabel @zTitle: @zLabel"
+        # )
+        # ser.setMeshSmooth(True)
+        # ser.dataProxy().addItems(array)
 
         for _ser in self.__scatter.seriesList():
             self.__scatter.removeSeries(_ser)
 
-        self.__scatter.addSeries(ser)
+        for ser in series:
+            self.__scatter.addSeries(ser)
+
+
+def group_by_indices(ar):
+    isort = np.argsort(ar)
+    ar = ar[isort]
+    inverse = np.empty_like(isort)
+    inverse[isort] = np.arange(isort.size)
+    diff = np.empty(isort.shape, dtype=np.bool_)
+    diff[0] = True
+    np.not_equal(ar[:-1], ar[1:], out=diff[1:])
+    uidx = np.flatnonzero(diff)
+    unq = ar[uidx]
+    if unq.size == 0:
+        return [], []
+    elif unq.size == 1:
+        return unq, [inverse]
+    else:
+        return unq, np.split(inverse, uidx[1:])
+
+
+from numbers import Integral
+from typing import Union
+
+
+def get_column_data(data, column, dtype=None):
+    # type: (Orange.data.Table, Union[int, Orange.data.Variable]) -> np.ma.MaskedArray
+    d, isview = data.get_column_view(column)
+    var = data.domain[column] # type: Orange.data.Variable
+
+    if var.is_primitive and not np.issubdtype(d.dtype, np.inexact):
+        d = d.astype(np.float)
+
+    if var.is_primitive():
+        pass
 
 
 def main(argv=None):
